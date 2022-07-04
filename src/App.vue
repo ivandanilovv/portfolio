267dff7e2a7d192e85cd325ebec29be8ed33e895
Ivan <ivandanilov282@gmail.com>
2022-07-04 12:59:59 +0200

    Navbar componnent

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <Navbar></Navbar>
     <router-view/>
   </div>
 </template>
+
 
 <style lang="scss">
 #app {
@@ -16,7 +14,6 @@
   text-align: center;
   color: #2c3e50;
 }
-
 nav {
   padding: 30px;
 
@@ -30,3 +27,10 @@ nav {
   }
 }
 </style>
+<script>
+  import Navbar from "@/components/layout/Navbar";
+  export default {
+    components: {Navbar}
+  }
+
+</script>
