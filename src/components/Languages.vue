1f5af951e088cce1f7d9ffbcd8b328bf6885b642
Ivan <ivandanilov282@gmail.com>
2022-07-06 14:54:38 +0200

    Updated progress bars

--- src/components/Languages.vue
+++ src/components/Languages.vue
@@ -3,20 +3,15 @@
     <h2 class="ps-lg-2 fw-bold headings-color"><i class="fa fa-language" aria-hidden="true"></i>
       Languages
     </h2>
-    <h6 class="fst-italic grey-color">
-      Macedonian(Native)
-    </h6>
-    <div class="progress">
-      <div class="progress-bar progress-bar-striped progress-bar-color" role="progressbar" style="width: 100%"
-           aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">Native
-      </div>
-    </div>
-    <h6 class="fst-italic grey-color">
-      English
-    </h6>
-    <div class="progress">
-      <div class="progress-bar progress-bar-color progress-bar-striped" role="progressbar" style="width: 85%"
-           aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">Fluent
+    <div v-for="language in progressBarLanguages">
+      <h6 class="fst-italic grey-color">
+        {{ language.languageName }}
+      </h6>
+      <div class="progress">
+        <div class="progress-bar progress-bar-striped progress-bar-color" role="progressbar"
+             :style="{width: language.progressBarWidth}" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+          {{ language.progressBarDescription }}
+        </div>
       </div>
     </div>
   </section>
@@ -24,7 +19,23 @@
 
 <script>
 export default {
-  name: "Languages"
+  name: "Languages",
+  data() {
+    return {
+      progressBarLanguages: [
+        {
+          languageName: 'Macedonian(Native)',
+          progressBarWidth: '100%',
+          progressBarDescription: 'Native'
+        },
+        {
+          languageName: 'English',
+          progressBarWidth: '85%',
+          progressBarDescription: 'Fluent'
+        }
+      ]
+    }
+  }
 }
 </script>
 
